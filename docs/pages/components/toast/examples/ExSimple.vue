1cea5a68f3d0dd930faf40696a5f265e37b35bf7
emacle <qkbeyond@gmail.com>
2020-11-30 09:45:42 +0800

    Clone from buefy@0.9.4 b-table-pagination 添加 b-pagination 的 order 属性, 前一页按钮可放在最前面

---
+++ docs/pages/components/toast/examples/ExSimple.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+        <div class="buttons">
+            <b-button
+                label="Launch toast (default)"
+                size="is-medium"
+                @click="toast" />
+            <b-button
+                label="Launch toast (custom)"
+                type="is-success"
+                size="is-medium"
+                @click="success" />
+            <b-button
+                label="Launch toast (custom)"
+                type="is-danger"
+                size="is-medium"
+                @click="danger" />
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        methods: {
+            toast() {
+                this.$buefy.toast.open('Something happened')
+            },
+            success() {
+                this.$buefy.toast.open({
+                    message: 'Something happened correctly!',
+                    type: 'is-success'
+                })
+            },
+            danger() {
+                this.$buefy.toast.open({
+                    duration: 5000,
+                    message: `Something's not good, also I'm on <b>bottom</b>`,
+                    position: 'is-bottom',
+                    type: 'is-danger'
+                })
+            }
+        }
+    }
+</script>
---
+++ types/test/test.vue
@@ -0,0 +1,15 @@
+<script lang='ts'>
+import Vue from 'vue';
+
+export default Vue.extend({
+    created(): any {
+        this.$buefy.dialog
+    },
+    methods: {
+        test(): string {
+            console.log(this);
+            return '';
+        }
+    }
+})
+</script>
